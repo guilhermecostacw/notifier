d67bd70652738430857b425cd22054e583b4a230
guilhermecostacw <guilherme.costa@cloudwalk.io>
2024-06-08 11:49:23 -0300

    add database config for tests

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
+  before do
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+  end
+
   describe 'GET /index' do
     it 'returns a success response' do
       get customers_path
